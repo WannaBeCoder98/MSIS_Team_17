e281183b343a73f36b47c9cb8c52226e9f61ae6d
jfolz <jackfolz98@gmail.com>
2021-10-21 16:28:44 -0400

    adding all necessary APIs and script. Additional HTML tweaks

---
+++ app/public/js/index.js
@@ -0,0 +1,133 @@
+const refApp = {
+    data() {
+        return {
+            games:[],
+            infoForm: {},
+            referees:[],
+            infoFormReferees: {},
+            assignments:[],
+            infoFormAssignments: {}
+        }
+    },
+    computed: {},
+    methods: {
+
+
+        // Game Script
+        fetchGameData() {
+            fetch('/api/games/index.php')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.games = responseJson;
+            })
+        },
+
+        postNewGame(evt) {   
+            
+            console.log("Posting!", this.infoForm);
+    
+            fetch('api/games/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.infoForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.games = json;
+                
+                // reset the form
+                this.resetinfoForm();
+              });
+        },
+        resetinfoForm() {
+
+            this.infoForm = {};
+        },
+
+        // Ref Script
+        fetchRefData() {
+            fetch('/api/referees/index.php')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+            })
+        },
+
+        postNewRef(evt) {   
+            
+            console.log("Posting!", this.infoFormReferees);
+    
+            fetch('api/referees/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.infoFormReferees),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referees = json;
+                
+                // reset the form
+                this.resetinfoFormRef();
+              });
+        },
+        resetinfoFormRef() {
+
+            this.infoFormReferees = {};
+        },
+        
+        // Assignment Script
+        fetchAssignmentData() {
+            fetch('/api/assignments/index.php')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.assignments = responseJson;
+            })
+        },
+
+        postNewAssignment(evt) {   
+            
+            console.log("Posting!", this.infoFormAssignments);
+    
+            fetch('api/assignments/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.infoFormAssignments),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.assignments = json;
+                
+                // reset the form
+                this.resetinfoFormAssignment();
+              });
+        },
+        resetinfoFormAssignment() {
+
+            this.infoFormAssignments = {};
+        }
+        
+    },
+    created(){
+        this.fetchGameData();
+        this.fetchRefData();
+        this.fetchAssignmentData();
+    }
+
+}
+
+Vue.createApp(refApp).mount('#refApp');
